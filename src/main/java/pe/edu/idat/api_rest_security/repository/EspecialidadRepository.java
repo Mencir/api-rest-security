b388eaef9f13a21d9ebac2b4675543c04abf0a1d
Mencir <smithpalomares4@gmail.com>
2025-08-05 09:59:45 -0500

    Listado de especialidades

--- src/main/java/pe/edu/idat/api_rest_security/repository/EspecialidadRepository.java
+++ src/main/java/pe/edu/idat/api_rest_security/repository/EspecialidadRepository.java
@@ -1,4 +1,7 @@
 package pe.edu.idat.api_rest_security.repository;
 
-public interface EspecialidadRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.idat.api_rest_security.model.Especialidad;
+
+public interface EspecialidadRepository extends JpaRepository<Especialidad, String> {
 }
